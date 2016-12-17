dbde26b8efa5ed52860737c770838466f7cd3e1b
Kota Ishimoto <ishikota086@gmail.com>
2016-12-17 18:13:45 +0900

    Add silent mode on play game method

--- pypokerai/utils.py
+++ pypokerai/utils.py
@@ -2,7 +2,7 @@ from pypokerai.player import PokerPlayer, ConsolePlayer
 from pypokerai.task import TexasHoldemTask, blind_structure
 from pypokerengine.api.game import setup_config, start_poker
 
-def play_game(max_round, names, value_functions, with_me=False):
+def play_game(max_round, names, value_functions, with_me=False, verbose=1):
     if with_me:
         assert len(value_functions) == 9
     else:
@@ -11,8 +11,8 @@ def play_game(max_round, names, value_functions, with_me=False):
     config = setup_config(max_round=max_round, initial_stack=10000, small_blind_amount=25)
     config.set_blind_structure(blind_structure)
     for name, value_func in zip(names, value_functions):
-        config.register_player(name, PokerPlayer(task, value_func))
+        config.register_player(name, PokerPlayer(task, value_func, debug=verbose==1))
     if with_me:
         config.register_player("console", ConsolePlayer())
-    return start_poker(config, verbose=1)
+    return start_poker(config, verbose=verbose)
 

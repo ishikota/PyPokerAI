97d3da1ea671dece66523fa652daa662596e2784
Kota Ishimoto <ishikota086@gmail.com>
2016-12-18 13:28:47 +0900

    Improve round_robin script

--- scripts/round_robin_match.py
+++ scripts/round_robin_match.py
@@ -10,6 +10,7 @@ sys.path.append(root)
 sys.path.append(src_path)
 
 from datetime import datetime
+import time
 import re
 import random
 import matplotlib
@@ -19,8 +20,8 @@ from pypokerai.utils import play_game
 from pypokerai.value_function import LinearModelOnehotFeaturesValueFunction
 from pypokerai.task import blind_structure
 
-MAX_ROUND = 2
-NB_MATCH = 100
+MAX_ROUND = 1
+NB_MATCH = 3000
 CHOICE_ALGORITHM = "random"
 LATEST_DIR = "gpi_finished"
 CHECKPOINT_PATH = os.path.join(os.path.dirname(__file__), "checkpoint")
@@ -30,32 +31,42 @@ tmp = LinearModelOnehotFeaturesValueFunction(blind_structure)
 tmp.setup()
 handicappers = tmp.handicappers
 
-# generate value functions
-load_fnames = [
-        'after_50000_iteration',
-        'after_100000_iteration',
-        'after_150000_iteration',
-        'after_200000_iteration',
-        'after_250000_iteration',
-        'after_300000_iteration',
-        'after_350000_iteration',
-        'after_400000_iteration',
-        'after_450000_iteration',
-        'after_500000_iteration'
-]
+load_paths = [
+        os.path.join(CHECKPOINT_PATH, "after_50000_iteration"),
+        os.path.join(CHECKPOINT_PATH, "after_100000_iteration"),
+        os.path.join(CHECKPOINT_PATH, "after_150000_iteration"),
+        os.path.join(CHECKPOINT_PATH, "after_200000_iteration"),
+        os.path.join(CHECKPOINT_PATH, "after_250000_iteration"),
+        os.path.join(CHECKPOINT_PATH, "after_300000_iteration"),
+        os.path.join(CHECKPOINT_PATH, "after_350000_iteration"),
+        os.path.join(CHECKPOINT_PATH, "after_400000_iteration"),
+        os.path.join(CHECKPOINT_PATH, "after_450000_iteration"),
+        os.path.join(CHECKPOINT_PATH, "after_500000_iteration"),
+        ]
+names = [
+        "after_50000_iteration",
+        "after_100000_iteration",
+        "after_150000_iteration",
+        "after_200000_iteration",
+        "after_250000_iteration",
+        "after_300000_iteration",
+        "after_350000_iteration",
+        "after_400000_iteration",
+        "after_450000_iteration",
+        "after_5000000_iteration",
+        ]
 
 value_funcs = []
-names = []
-for fname in load_fnames:
+for name, path in zip(names, load_paths):
     f = LinearModelOnehotFeaturesValueFunction(blind_structure, handicappers)
     f.setup()
-    f.load(os.path.join(CHECKPOINT_PATH, fname))
+    f.load(path)
     value_funcs.append(f)
-    names.append(fname)
 
 result_holder = { name: [] for name in names }
 assert len(result_holder.keys()) == 10
 
+start_time = time.time()
 for i in range(NB_MATCH):
     zipped = zip(names, value_funcs)
     random.shuffle(zipped)
@@ -65,6 +76,7 @@ for i in range(NB_MATCH):
     for player_info in result["players"]:
         result_holder[player_info["name"]].append(player_info["stack"])
     print "%d / %d finished" % (i+1, NB_MATCH)
+print "round-robin %d times took %s sec" % (NB_MATCH, time.time()-start_time)
 
 # Create output directory if needed
 OUTPUT_DIR = os.path.join(os.path.dirname(__file__), "game_results")
@@ -83,6 +95,7 @@ for key, vals in result_holder.items():
     plt.plot(X, vals, label=str(fetch_num(key)))
 plt.legend(fontsize=8)
 plt.savefig(plot_save_path)
+print "1-pane graph saved on [ %s ]" % plot_save_path
 
 # draw each graph on sub pane
 def plot_title(title, x=NB_MATCH/2, y=50000):
@@ -98,4 +111,5 @@ for idx, key in enumerate(keys):
     plt.ylim(0, 100000)
     plt.plot(X, vals)
 plt.savefig(sub_plot_save_path)
+print "subplot graph saved on [ %s ]" % sub_plot_save_path
 

6fe4bf3858a45addf2512513bceadade5bff6f71
Kota Ishimoto <ishikota086@gmail.com>
2016-12-20 12:23:55 +0900

    Improve round-robin script to show histgram

--- scripts/round_robin_match.py
+++ scripts/round_robin_match.py
@@ -20,11 +20,14 @@ from pypokerai.utils import play_game
 from pypokerai.value_function import LinearModelOnehotFeaturesValueFunction
 from pypokerai.task import blind_structure
 
-MAX_ROUND = 1
+MAX_ROUND = None
 NB_MATCH = 3000
-CHOICE_ALGORITHM = "random"
 LATEST_DIR = "gpi_finished"
 CHECKPOINT_PATH = os.path.join(os.path.dirname(__file__), "checkpoint")
+
+# validatioin
+if not MAX_ROUND:
+    raise Exception("You forget to set max round for n-round poker task.")
 
 # generate handicappers
 tmp = LinearModelOnehotFeaturesValueFunction(blind_structure)
@@ -83,33 +86,42 @@ OUTPUT_DIR = os.path.join(os.path.dirname(__file__), "game_results")
 if not os.path.exists(OUTPUT_DIR):
     os.mkdir(OUTPUT_DIR)
 
-# generate graph
+# generate graph from here
 time_stamp = datetime.now().strftime('%m%d_%H_%M_%S')
 num_matcher = re.compile("after_(\d+)_iteration")
 fetch_num = lambda fname: int(num_matcher.search(fname).groups()[0])
 X = range(1, NB_MATCH+1)
-
-# draw all graph on 1 pane
-plot_save_path = os.path.join(OUTPUT_DIR, "%d_round_%d_match_plot_%s.png" % (MAX_ROUND, NB_MATCH, time_stamp))
-for key, vals in result_holder.items():
-    plt.plot(X, vals, label=str(fetch_num(key)))
-plt.legend(fontsize=8)
-plt.savefig(plot_save_path)
-print "1-pane graph saved on [ %s ]" % plot_save_path
+original_font_size = plt.rcParams["font.size"]
 
 # draw each graph on sub pane
-def plot_title(title, x=NB_MATCH/2, y=50000):
-    plt.text(x, y, title, alpha=0.5, size=10, ha="center", va="center")
-
+plt.rcParams["font.size"] = 7
 sub_plot_save_path = os.path.join(OUTPUT_DIR, "%d_round_%d_match_subplot_%s.png" % (MAX_ROUND, NB_MATCH, time_stamp))
 keys = sorted(result_holder.keys(), key=fetch_num)
-for idx, key in enumerate(keys):
+fig = plt.figure()
+for i, key in enumerate(keys):
     vals = result_holder[key]
     avg = 1.0 * sum(vals)/NB_MATCH
-    plt.subplot(5, 2, idx+1)
-    plot_title("%s (%s)" % (key, avg))
-    plt.ylim(0, 100000)
-    plt.plot(X, vals)
+    ax = fig.add_subplot(5, 2, i+1)
+    ax.title.set_text("%s (avg=%.2f)" % (key, avg))
+    ax.set_ylim(0, 100000)
+    ax.plot(X, vals)
+fig.tight_layout()
 plt.savefig(sub_plot_save_path)
+plt.rcParams["font.size"] = original_font_size
 print "subplot graph saved on [ %s ]" % sub_plot_save_path
+
+# draw histgram
+hist_save_path = os.path.join(OUTPUT_DIR, "%d_round_%d_match_histgram_%s.png" % (MAX_ROUND, NB_MATCH, time_stamp))
+plt.rcParams["font.size"] = 7
+keys = sorted(result_holder.keys(), key=fetch_num)
+fig = plt.figure()
+for i, key in enumerate(keys):
+    vals = result_holder[key]
+    ax = fig.add_subplot(5, 2, i+1)
+    ax.title.set_text(key)
+    ax.hist(vals, bins=30)
+fig.tight_layout()
+plt.savefig(hist_save_path)
+plt.rcParams["font.size"] = original_font_size
+print "histgram saved on [ %s ]" % hist_save_path
 
